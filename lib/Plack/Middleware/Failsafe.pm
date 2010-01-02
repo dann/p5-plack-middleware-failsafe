ed3f2b6eaa9b3baa9b9ded327df37583c6d115e4
Dann <techmemo@gmail.com>
2010-01-03 01:03:57 +0900

    initial commit

---
+++ lib/Plack/Middleware/Failsafe.pm
@@ -0,0 +1,47 @@
+package Plack::Middleware::Failsafe;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Plack::Middleware::Failsafe -
+
+=head1 SYNOPSIS
+
+  use Plack::Middleware::Failsafe;
+
+=head1 DESCRIPTION
+
+Plack::Middleware::Failsafe is
+
+
+=head1 SOURCE AVAILABILITY
+
+This source is in Github:
+
+  http://github.com/dann/
+
+=head1 CONTRIBUTORS
+
+Many thanks to:
+
+
+=head1 AUTHOR
+
+dann E<lt>techmemo@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
